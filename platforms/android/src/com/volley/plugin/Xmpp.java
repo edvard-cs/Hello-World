b5cbf9066a07e81a7b27fd70bea91f577e32e370
edvard_cs <edvard_122@hotmail.com>
2015-01-27 18:00:39 -0800

    add pacage com.volley.plugin

---
+++ platforms/android/src/com/volley/plugin/Xmpp.java
@@ -0,0 +1,14 @@
+package com.volley.plugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+
+public class Xmpp extends CordovaPlugin {
+
+	public boolean execute(final String action, final JSONArray args,
+      final CallbackContext callbackContext) {
+		
+    	return true;  	
+    }
+}
